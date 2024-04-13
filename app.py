9c74043712f3ebc44c0a26dd1fcb049d408537c4
martinmballe <131255317+Ertmartinert@users.noreply.github.com>
2024-04-14 00:06:35 +0200

    I got this cheesburgerman

--- app.py
+++ app.py
@@ -75,4 +75,5 @@ def upload_file():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv('PORT', 5000))  # Default port to 5000 if PORT not set
+    app.run(host='0.0.0.0', port=port, debug=False)  # Turn off debug for production
